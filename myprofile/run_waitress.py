3afd6acf553125f86e54467b3d1f6cfe8edfc20a
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-07-05 07:00:51 +0530

    Update run_waitress.py

--- myprofile/run_waitress.py
+++ myprofile/run_waitress.py
@@ -1,6 +1,6 @@
 import os
 from waitress import serve
-from NTI.wsgi import application
+from myprofile.wsgi import application
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myprofile.settings')
